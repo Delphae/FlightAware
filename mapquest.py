962bd7000923a063df38369f1dcd9846b708faa8
Delphae <45516472+Delphae@users.noreply.github.com>
2018-12-01 21:19:12 +0100

    Add files via upload

--- mapquest.py
+++ mapquest.py
@@ -2,20 +2,30 @@
 # coding: utf-8
 
 '''
-Open Geocoding API - MAPQUEST
+Looking for Latitude and Longitude for a location
+Uses Open Geocoding API - MAPQUEST
 https://developer.mapquest.com
 
 '''
 import requests
-APIKEY = 'MAPQUEST APIKEY'
+APIKEY = 'NOwxJonypq7z92AZbftbATxhyrOpzBGF'
+MAXRESULTS = 10
 
 city = 'Amsterdam'
-url = 'http://www.mapquestapi.com/geocoding/v1/address?key=%s&location=%s&outFormat=json&maxResults=1' % (APIKEY, city)
 
-response = requests.get(url)
+url = 'http://www.mapquestapi.com/geocoding/v1/address'
+params = {'location':city,
+          'outFormat':'json',
+          'maxResults':MAXRESULTS,
+          'key':APIKEY}
+
+response = requests.get(url, params=params)
 mapquest = response.json()
 results = mapquest['results']
-locations = results[0]['locations']
 
-location = locations[0]
-print location['latLng']['lat'], location['latLng']['lng']
+for result in results:
+    locations = result['locations']
+    for location in locations:
+        print ('%4s %12f %12f' % (location['adminArea1'],
+                                 location['latLng']['lat'],
+                                 location['latLng']['lng']))
